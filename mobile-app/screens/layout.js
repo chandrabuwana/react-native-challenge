36cb5c23d64bc2ec62a15073dc4b08a90b36818e
chandrabuwana <cbuwana@gmail.com>
2017-12-20 20:22:37 +0700

    done detail

--- mobile-app/screens/layout.js
+++ mobile-app/screens/layout.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-
+import { Text, View, Image } from 'react-native'
+import { Card, ListItem, Button, List } from 'react-native-elements'
 export default class ChatScreen extends React.Component {
   // // Nav options can be defined as a function of the screen's props:
   
   render() {
     // The screen's current route is passed in to `props.navigation.state`:
     const { params } = this.props.navigation.state;
-    // console.log('ini props dari this.props',this.props.navigation.state)
+    console.log('ini props dari this.props',this.props.navigation.state)
     return (
       <View>
-        <Text>ini Chatscreen</Text>
+        <Card title={params.itemDetail.title}>
+          <Image 
+            style={{ height: 100 }} resizeMode="contain"
+            source={{uri : params.itemDetail.urlToImage}}
+            />
+          <Text>Author: {params.itemDetail.author}</Text>
+          <Text >{params.itemDetail.description}</Text>
+        </Card>
+        
       </View>
     );
   }
